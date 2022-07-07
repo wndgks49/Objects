b7fb9c1ac60a137463d1c6a6ea242ff1a1ebbe1c
wndgks49 <89828897+wndgks49@users.noreply.github.com>
2022-07-07 10:30:17 +0900

    fix audience

--- src/main/java/makeTicketSeller/Audience.java
+++ src/main/java/makeTicketSeller/Audience.java
@@ -8,8 +8,18 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag(){
-        return bag;
+//    public Bag getBag(){
+//        return bag;
+//    } 가방은 관람객으로부터 접근을 차단했음. 결론적으로 bag의 내부적으로 캡슐화 성공함.
+    public Long buy(Ticket ticket){
+        if(bag.hasInvitation()){
+            bag.setTicket(ticket);
+            return 0L;
+        }else{
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 
 
--- src/main/java/makeTicketSeller/TicketSeller.java
+++ src/main/java/makeTicketSeller/TicketSeller.java
@@ -13,17 +13,7 @@ public class TicketSeller {
 //    } 티켓 판매자가 더이상 티켓 오피스로부터 자율성을 얻게되었다.  티켓 오피스로부터 접근을 막았고 이것을 캡슐화 라고 한다.
 
 
-    public void sellTo(Audience audience){
-     if(audience.getBag().hasInvitation()){
-         Ticket ticket = ticketOffice.getTicket();
-         audience.getBag().setTicket(ticket);
-     }else{
-         Ticket ticket = ticketOffice.getTicket(); //티켓이 존재하고
-         audience.getBag().minusAmount(ticket.getFee()); // 관람객이 돈을 지불하고
-         ticketOffice.plusAmount(ticket.getFee());// 사무실이 금액이 올라가고
-         audience.getBag().minusAmount(ticket.getFee()); // 관람객은 돈이 빠져나가고
-         audience.getBag().setTicket(ticket); // 관람객은 티켓을  받는다.
-
-     }
+    public void sellTo(Audience audience){ // 객체 접근을 막고 메소드로 서로 알기만 해주기.
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
